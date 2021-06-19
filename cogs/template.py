a98e7f4c8f919c7062b67146700f9a47be5afcd3
rlp81 <58034254+rlp81@users.noreply.github.com>
2021-06-19 14:12:29 -0500

    Update template.py

--- cogs/template.py
+++ cogs/template.py
@@ -1,1 +1,11 @@
+import discord
+from discord.ext import commands
 
+class template(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+    
+
+def setup(client):
+    client.add_cog(template(client))
